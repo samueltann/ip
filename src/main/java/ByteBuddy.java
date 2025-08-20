ae22b8467f7de2eeaae2c5f65a1fc54f48fd9ecc
Samuel <samueltanwj@gmail.com>
2025-08-20 16:36:55 +0800

    Add support for echo

--- src/main/java/ByteBuddy.java
+++ src/main/java/ByteBuddy.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class ByteBuddy {
+    public static final String line = "____________________________________________________________";
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
+        Scanner scanner = new Scanner(System.in);
+        printGreeting();
+        String input;
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equalsIgnoreCase("bye")) {
+                printFarewell();
+                break;
+            }
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
+        scanner.close();
+    }
+    private static void printGreeting() {
         System.out.println(line);
         System.out.println("Hello! I'm ByteBuddy\n" + "What can I do for you?");
+        System.out.println(line);
+    }
+
+    private static void printFarewell() {
         System.out.println(line);
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
