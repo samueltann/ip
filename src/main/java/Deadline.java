87092ca08a18c9512372e512e3d7a8851fe79d86
Samuel <samueltanwj@gmail.com>
2025-08-22 18:52:03 +0800

    Add enum for TaskType

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -3,12 +3,12 @@ public class Deadline extends Task {
     protected String by;
 
     public Deadline(String description, String by) {
-        super(description);
+        super(description, TaskType.DEADLINE);
         this.by = by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        return "["+ taskType.getSymbol() + "]" + super.toString() + " (by: " + by + ")";
     }
 }
---
+++ src/main/java/TaskType.java
@@ -0,0 +1,12 @@
+public enum TaskType {
+    TODO("T"),
+    DEADLINE("D"),
+    EVENT("E");
+    private final String symbol;
+    TaskType(String symbol) {
+        this.symbol = symbol;
+    }
+    public String getSymbol() {
+        return symbol;
+    }
+}
