143048d0c1ebdbcdab5d7c9707ba58fb91de5f6d
Samuel <samueltanwj@gmail.com>
2025-08-22 14:53:35 +0800

    Add support for Todo, Event and Deadline

--- src/main/java/ByteBuddy.java
+++ src/main/java/ByteBuddy.java
@@ -40,12 +40,53 @@ public class ByteBuddy {
                     System.out.println("  " + tasks.get(index));
                     System.out.println(LINE);
                 }
+            } else if (input.startsWith("todo ")) {
+                String desc = input.substring(5).trim();
+                System.out.println(desc);
+                if (desc.isEmpty()) {
+                    printLineMsg("Error: The description of a todo cannot be empty.");
+                    continue;
+                }
+                Task t = new Todo(desc);
+                tasks.add(t);
+                printAdded(tasks, t);
+            } else if (input.startsWith("deadline ")) {
+                String body = input.substring(9).trim();
+                if (body.isEmpty()) {
+                    printLineMsg("Error: The description of a deadline cannot be empty.");
+                    continue;
+                }
+                int byIdx = body.indexOf("/by");
+                if (byIdx == -1) {
+                    printLineMsg("Error:  Please provide a description and a '/by <time>'.");
+                }
+                String desc = body.substring(0, byIdx).trim();
+                String by = body.substring(byIdx + 3).trim();
+                Task t = new Deadline(desc, by);
+                tasks.add(t);
+                printAdded(tasks, t);
+            } else if (input.startsWith("event ")) {
+                String body = input.substring(6).trim();
+                if (body.isEmpty()) {
+                    printLineMsg("Error: The description of a event cannot be empty.");
+                    continue;
+                }
+                int fromIdx = body.indexOf("/from");
+                if (fromIdx == -1) {
+                    printLineMsg("Error:  Please provide a description and a '/from ... /to ...'");
+                }
+                int toIdx = body.indexOf("/to");
+                if (toIdx == -1) {
+                    printLineMsg("Error:  Please provide a description and a '/from ... /to ...'");
+                }
+                String desc = body.substring(0, fromIdx).trim();
+                String from = body.substring(fromIdx + 5, toIdx).trim();
+                String to = body.substring(toIdx + 3).trim();
+                Task t = new Event(desc, from, to);
+                tasks.add(t);
+                printAdded(tasks, t);
             } else {
-                Task newTask = new Task(input);
-                tasks.add(newTask);
-                System.out.println(LINE);
-                System.out.println("added: " + input);
-                System.out.println(LINE);
+                printLineMsg("Error: Please enter command todo / deadline / event");
             }
         }
 
@@ -63,4 +104,19 @@ public class ByteBuddy {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(LINE);
     }
+
+    private static void printAdded(ArrayList<Task> tasks, Task t) {
+        System.out.println(LINE);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("  " + t);
+        System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+        System.out.println(LINE);
+    }
+
+    private static void printLineMsg(String msg) {
+        System.out.println(LINE);
+        System.out.println(msg);
+        System.out.println(LINE);
+    }
+
 }
