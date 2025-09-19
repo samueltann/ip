2f0d6099f5a2f32b8d19ff7af669ee7471168804
Samuel <samueltanwj@gmail.com>
2025-09-19 13:36:41 +0800

    Move tasks.txt from src/main/data to data/

--- src/main/java/bytebuddy/gui/Main.java
+++ src/main/java/bytebuddy/gui/Main.java
@@ -14,7 +14,7 @@ import javafx.stage.Stage;
  */
 public class Main extends Application {
 
-    private final ByteBuddy byteBuddy = new ByteBuddy("src/main/data/tasks.txt");
+    private final ByteBuddy byteBuddy = new ByteBuddy("data/tasks.txt");
 
     @Override
     public void start(Stage stage) {
